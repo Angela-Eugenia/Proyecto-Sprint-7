b53bdfffc4f88408e8eb3f171fe322cc0c5f163e
Angela-Eugenia <angelareyesosorio@gmail.com>
2026-01-17 16:26:11 -0500

    Enhance visualizations by adding axis labels and adjusting ranges for histograms and scatter plot

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Proyecto-Sprint-7
 Pagina de coches
+Analisis sobre data de vehiculos y sus caracteristicas
+Link, pagina Internet    https://proyecto-sprint-7-1-ceqx.onrender.com
+
--- ddd.py
+++ ddd.py
@@ -2,6 +2,8 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# Base con información sobre vehiclos, para analisis
+
 datos= pd.read_csv('vehicles_us.csv')
 
 st.title('Autos para la venta')
@@ -12,6 +14,7 @@ boton1 = st.checkbox('Construir gráfica', key="precio")
 if boton1:
     st.write('Histograma por precio')
     grafico1 = px.histogram(datos, x='price')
+    grafico1.update_xaxes(range=[0, 150000])
     st.plotly_chart(grafico1, width="stretch")
 
 st.header('Estado del vehículo')
@@ -20,7 +23,8 @@ st.header('Estado del vehículo')
 boton2 = st.checkbox('Construir gráfica', key="kilometraje")
 if boton2:
     st.write('Histograma kilometraje')
-    grafico2 = px.histogram(datos, x="odometer")
+    grafico2 = px.histogram(datos, x="odometer", labels={'odometer': 'kilometraje (KM)'})
+    grafico2.update_xaxes(range=[0, 0.6])
     st.plotly_chart(grafico2, width="stretch")
 
 st.header('Relación precio - kilometraje')
@@ -33,7 +37,10 @@ if boton3:
         datos,
         x="odometer",
         y="price",
+        labels='kilometraje(KM)',
         title='Precio vs kilometraje',
         color_discrete_sequence=["green"]
+        
     )
+    grafico3.update_xaxes(range=[0, 0.6])
     st.plotly_chart(grafico3, width="stretch")
